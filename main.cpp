898605e620420cfca30fdba5680c26085d869c6d
silveira.kaue@gmail.com <silveira.kaue@gmail.com@885d560e-e0f9-11de-8065-53a27c5290e0>
2009-12-04 19:11:46 +0000

    - test

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: pet
+ *
+ * Created on 4 de Dezembro de 2009, 17:12
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
