59c6f233f78e8e36151dd842e270754bfc25be83
silveira.kaue@gmail.com <silveira.kaue@gmail.com@885d560e-e0f9-11de-8065-53a27c5290e0>
2010-01-22 18:46:06 +0000

    Commiting all robopet. Communication done. All the arrows working. Did some corrections of the makefile of the robopet-communication and created the makefile of the gui.

--- main.cpp
+++ main.cpp
@@ -4,49 +4,16 @@
 #include "ssl_server.h"
 
 RoboCupSSLServer simtotracker(PORT_SIM_TO_TRACKER, IP_SIM_TO_TRACKER);
-RoboCupSSLClient aitosim(PORT_AI_TO_SIM, IP_SIM_TO_TRACKER);
+RoboCupSSLClient aitosim(PORT_AI_TO_SIM, IP_AI_TO_SIM);
 
 int DEBUG = 1;
 
 void receive()
 {
 	SSL_WrapperPacket packet;
-	if (aitosim.receive(packet)) {
-		printf("----------------------------\n");
-		printf("TestClient Received ");
-
-		if (packet.has_aitosim())
-		{
-			printf("AI-To-SIM!\n");
-		}
-
-/*
-		if (packet.has_trackertoai())
-		{
-			if(DEBUG)
-			{
-				printf("Tracker-To-AI!\n");
-				printf("Ball: <%d, %d>\n", packet.trackertoai().ball().x(),
-				 							packet.trackertoai().ball().y());
-			
-			
-				for(int i=0; i<packet.trackertoai().robots_blue_size(); i++)
-					printf("Blue Robot[%d]: <%d, %d>\n", i,
-											packet.trackertoai().robots_blue(i).x(),
-											packet.trackertoai().robots_blue(i).y());
-				for(int i=0; i<packet.trackertoai().robots_yellow_size(); i++)
-					printf("Yellow Robot[%d]: <%d, %d>\n", i,
-											packet.trackertoai().robots_yellow(i).x(),
-											packet.trackertoai().robots_yellow(i).y());
-			}
-			
-			_ball.setCurrentPosition(Point(packet.trackertoai().ball().x(),
-				 						   packet.trackertoai().ball().y()));
-				 						   
-			for(int i=0; i<packet.trackertoai().robots_blue_size(); i++)
-				_players[i].setCurrentPosition(Point(packet.trackertoai().robots_blue(i).x(),
-												     packet.trackertoai().robots_blue(i).y()));
-		}//*/
+	if (aitosim.receive(packet) && packet.has_aitosim()) {
+		printf("----------------------------");
+		printf("Received AI-To-SIM!\n");
 	}
 }
 
@@ -56,23 +23,24 @@ void send()
 
 	 SimToTracker *simtotrackerPacket = packet.mutable_simtotracker();
 	 BallSim *b = simtotrackerPacket->mutable_ball();
-	 
+
 	 b->set_x(77);
 	 b->set_y(666);
-	 
+
 	 simtotracker.send(packet);
+	printf("Sent Sim-To-Tracker\n");
 }
 
 int main()
 {
 	printf("Simulator Running!\n");
-	
+
 	aitosim.open(false);
-	
+
 	printf("Press <Enter> to open connection with client...\n");
 	getchar();
 	simtotracker.open();
-	
+
 	while(1) {
 		receive();
 		send();
