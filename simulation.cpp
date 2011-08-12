b24527cdebf1e7a3a9c88cae8a83c87961bee277
vepuha <vepuha@885d560e-e0f9-11de-8065-53a27c5290e0>
2011-08-12 02:58:44 +0000

    Commiting all robopet. Improved function twoBarrier, almost robots don't get out the field and don't get in the area, referee is now reciving all signals and testing it

--- simulation.cpp
+++ simulation.cpp
@@ -32,8 +32,8 @@
 
 float MOTOR_FORCE = .0625;
 
-float ROBOT_DENSITY = 0.005;
-float BALL_DENSITY = 0.002; // the ball should weigh approximately 46 g
+float ROBOT_DENSITY = 0.004;
+float BALL_DENSITY = 0.001; // the ball should weigh approximately 46 g
 float BALL_DAMP = .6; // 0 - 1
 float ROBOT_DAMP = 1;
 
